4a5e57a8a172efbaa9c53d5c039b36067cf9062f
Usama Uzair <usama19026@gmail.com>
2024-08-12 09:36:33 +0530

    test : addEmployee

--- frontend/e2eTest/employee/addEmployee.spec.ts
+++ frontend/e2eTest/employee/addEmployee.spec.ts
@@ -6,33 +6,18 @@ test.describe('AddNotice', () => {
         await mockApiRoute(page, '*/**/api/employee', {
             status: 201
         });
-        await page.goto('/employee/list');
+        await page.goto('/employee/add');
         await page.waitForLoadState('networkidle');
     });
 
     test('should render the form and submit new employee', async ({ page }) => {
 
-        await page.waitForTimeout(10000);
-        // Explicitly wait for the button to be available
-        const addEmployeeButton = page.locator('text=Add Employee');
-        await addEmployeeButton.waitFor({ state: 'visible', timeout: 10000 });
-
-        // Click the "Add Employee" button and wait for the navigation
-        await Promise.all([
-            page.waitForURL('/employee/add'),
-            addEmployeeButton.click()
-        ]);
-
-        await page.getByPlaceholder('First Name of the Employee').fill('Usamaa');
+        await page.getByPlaceholder('First Name of the Employee').fill('Usamaall');
         await page.getByPlaceholder('Last Name of the Employee').fill('Uzaair');
 
         await page.getByPlaceholder('Email of the Employee').fill('usama19026@gmail.com');
         await page.getByPlaceholder('Phone Number of the Employee').fill('0777609615');
+        await page.click('button:has-text("Create")');
 
-        // Select gender from the dropdown
-        await page.click('button:has-text("Select Gender")');
-        await page.click('text=Male');
-
-        await page.click('button[type="submit"]');
     });
 });
